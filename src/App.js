2745ee4ca84e9179ac6f4643e0092e8b3b3a08e7
monil <patelmonil7908@gmail.com>
2023-09-27 14:57:52 +0530

    save

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import React from "react";
 import Project from "./PR4/Project";
 import CountdownTimer from "./PR5/CountdownTimer";
 // import Card from "./PR4/Card";
-// import Header from "./PR3/components/Header";
-// import About from "./PR3/components/About";
-// import Count from "./PR3/components/Count";
-// import Services from "./PR3/components/Services";
-// import Menu from "./PR3/components/Menu";
-// import Chef from "./PR3/components/Chef";
-// import Reservation from "./PR3/components/Reservation";
-// import Customer from "./PR3/components/Customer";
-// import Footer from "./PR3/components/Footer";
+import Header from "./PR3/components/Header";
+import About from "./PR3/components/About";
+import Count from "./PR3/components/Count";
+import Services from "./PR3/components/Services";
+import Menu from "./PR3/components/Menu";
+import Chef from "./PR3/components/Chef";
+import Reservation from "./PR3/components/Reservation";
+import Customer from "./PR3/components/Customer";
+import Footer from "./PR3/components/Footer";
 const App = () => {
   return (
     <>
       {/* PR5 */}
 
-      <div className="App">
+      {/* <div className="App">
        <CountdownTimer initialTime={60} /> 
-     </div> 
+     </div>  */}
 
       {/* Website PR3 */}
-      {/* <Header />
+      <Header />
       <About />
       <Count />
       <Services />
@@ -30,7 +30,7 @@ const App = () => {
       <Chef />
       <Reservation />
       <Customer />
-      <Footer /> */}
+      <Footer />
 
     </>
   );
